81b0f03f34501e9dc2165d3be9e09f82a1a58edd
jbailis <123606976+jbailis@users.noreply.github.com>
2023-03-11 20:46:56 -0800

    Complete javascript exercises 06 - 07
    Apply newly learned data validation techniques.
    Practice number formatting.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  return validateLeapYear(year);
 };
+
+// validate leap year
+const runValidation = (validation, errMsg) => data =>
+  validation(data) ? true : errMsg;
+
+const validateLeapYear = runValidation(data =>
+  data % 4 === 0 && (data % 100 !== 0 || data % 400 === 0), 
+  false);
+
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return formatTemp((temp - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return formatTemp((temp * 9 / 5) + 32);
 };
 
+const formatTemp = function(temp) {
+  return Math.round(temp * 10) / 10;
+}
+  
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
